4b8ce4d35c7eaae848121950551b1cb5a3f76a8b
Robson Gil Rocha <gil1990gil@hotmail.com>
2021-03-04 21:00:52 -0300

    rota testada

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import styled from 'styled-components'
 
-function App() {
+const Row =styled.div`
+display:flex;
+align-items:center;
+justify-content:center;
+`
+const space =styled.div`
+
+`
+
+export const Container = styled.div`
+  
+`;
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <Row>
+              <Link to="/">Home</Link>
+              <space>///////////////</space>
+              <Link to="/about">About</Link>
+              <space>///////////////</space>
+              <Link to="/users">Users</Link>
+              <space>///////////////</space>
+              <Link to="/conct">Conct</Link>
+          </Row>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+         <Route path="/conct">
+            <Conct />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Pagina Inicial</h2>;
+}
+
+function About() {
+  return <h2>Sobre</h2>;
+}
+
+function Users() {
+  return <h2>Usuarios</h2>;
+}
+
+function Conct() {
+  return <h2>Contatos</h2>
+}
